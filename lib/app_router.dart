40333f4416eed8f8ae089924cc477f51673e6902
NaderOsama <naderosama665@gmail.com>
2022-12-05 13:25:32 +0200

    Upload Characters is Breaking Bad By Flutter

---
+++ lib/app_router.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_breaking/constants/constants.dart';
+import 'package:flutter_breaking/data/models/charactersModel/characters_model.dart';
+import 'cubit/characters_cubit.dart';
+import 'data/repository/characters_repository.dart';
+import 'data/web_services/characters_web_services.dart';
+import 'presentation/screens/characters_details_screen.dart';
+import 'presentation/screens/characters_screen.dart';
+
+class AppRouter {
+   CharactersRepository charactersRepository;
+   CharactersCubit charactersCubit;
+
+  AppRouter() {
+    charactersRepository = CharactersRepository(CharactersWebServices());
+    charactersCubit = CharactersCubit(charactersRepository);
+  }
+
+  Route generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case charactersScreen:
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (BuildContext contxt) => charactersCubit,
+            child: CharactersScreen(),
+          ),
+        );
+
+      case characterDetailsScreen:
+        final character = settings.arguments as Character;
+
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (BuildContext context) =>
+                CharactersCubit(charactersRepository),
+            child: CharacterDetailsScreen(
+              character: character,
+            ),
+          ),
+        );
+    }
+  }
+}
